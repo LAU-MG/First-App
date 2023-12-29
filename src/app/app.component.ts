46d3c48941f66e00e64520c6a33fb17ad6476941
Laura Miguel <laura.miguel.v2.4@gmail.com>
2023-12-29 17:23:50 +0100

    fin du projet'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import {RouterModule } from '@angular/router';
+import {RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [
-    HomeComponent, RouterModule
-  ],
   selector: 'app-root',
   standalone: true,
-  styleUrls: ['./app.component.css'],
+  imports: [
+    HomeComponent,
+    RouterLink,
+    RouterOutlet,
+  ],
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="../assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="../assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
       <section class="content">
         <router-outlet></router-outlet>
       </section>
     </main>
-  `
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
 }
+
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -6,16 +6,24 @@ import {RouterModule} from '@angular/router';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule, RouterModule],
-  template:`
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  template: `
     <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}}" >
-    <h2 class="listing-heading">{{housingLocation.name}}</h2>
-      <p class="listing-location">{{housingLocation.city}},{{housingLocation.state}}</p>
-    <a [routerLink]="['details',housingLocation.id]">Learn More</a>
-    </section>`,
-  styleUrls: ['./housing-location.component.css']
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css'],
 })
+
 export class HousingLocationComponent {
-  @Input() housingLocation!:HousingLocation;
+
+  @Input() housingLocation!: HousingLocation;
+
 }
+
